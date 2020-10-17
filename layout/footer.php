79bcd63f0e3a06edbdb92f285692cacb5f48edb8
Steven Andrews <osidesigns@gmail.com>
2020-10-17 09:00:18 -0500

    Add all file for initial commit

---
+++ layout/footer.php
@@ -0,0 +1,8 @@
+
+<footer>
+    <hr />
+    <p>&copy;Copyright2020 <span>PortfoTheme</span>. All Rights Reserved.</p>
+</footer>
+
+</body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,600;0,800;1,400&display=swap" rel="stylesheet">
+    <title>Portfolio Mockup</title>
+</head>
+<body>
+    
+<div id="headerWrapper">
+    <div id="topbar">
+        <div class="container">
+            <header>
+                <h1 id="branding">Now <span>&</span> Later</h1>
+                <div id="mobileNav">
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                </div>
+                <nav id="mainNav">
+                    <ul>
+                        <li><a href="#">item 1</a></li>
+                        <li><a href="#">item 2</a></li>
+                        <li><a href="#">item 3</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    </div>
+    <div class="container">
+        <div id="heroArea">
+            <h1>Welcome To The <br/><span class="part1">Portfo</span> <span class="part2">Theme</span></h1>
+            <p id="slogan">your future career in web development</p>
+        </div>
+    </div>
+</div>
